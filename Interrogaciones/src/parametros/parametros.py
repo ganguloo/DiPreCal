ef77dca83f69edfef2328448c9bdaff32795a8aa
ganguloo <64566122+ganguloo@users.noreply.github.com>
2024-07-05 11:58:08 -0400

    Update parametros.py

--- Interrogaciones/src/parametros/parametros.py
+++ Interrogaciones/src/parametros/parametros.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from datos.conversion_excel_parametros import fechas_prohibidas, excel_cursos_coordinados, mapeo_fechas, mapeo
+from datos.conversion_excel_parametros import fechas_prohibidas, excel_cursos_coordinados, mapeo_fechas, mapeo, fijar_interrogaciones
 
 
 
@@ -51,7 +51,7 @@ CURSOS_COORDINADOS = excel_cursos_coordinados(PATH_PARAMETROS, "Cursos Coordinad
 
 # Obtenemos las pruebas preasignadas (solo para cursos coordinados)
 #Formato: si cursos todos coordinados: (curso (str), fecha (int), nr de prueba(int)) ¡La fecha debe ser en línea de tiempo real!
-PRUEBAS_PREASIGNADAS = excel_cursos_coordinados(PATH_PARAMETROS, "Interrogaciones Fijas")
+PRUEBAS_PREASIGNADAS = fijar_interrogaciones(PATH_PARAMETROS, "Interrogaciones Fijas")
 
 DIA_FECHA_RETIRO_CURSOS = mapeo_fechas(PATH_PARAMETROS, "Fechas", "D1")
 DIA_I2 = mapeo_fechas(PATH_PARAMETROS, "Fechas", "F1")
@@ -67,24 +67,30 @@ DELTA_DIAS = 2
 
 SEC_COORDINADAS = [["ICS2123", "2", "3"], ["ICS3313", "2", "3"], ["ICS3413", "2", "3"]] #[["ICS3313","2","3"],["ICS3413","2","3"],["ICS2523","1","2"]]
 
-CURSOS_3_IES = ["MAT1640_Coordinado - Macroseccion","MAT1630_Coordinado - Macroseccion","MAT1620_Coordinado - Macroseccion","MAT1610_Coordinado - Macroseccion",
-                "MAT1203_Coordinado - Macroseccion","EYP1113_Coordinado - Macroseccion"]
+CURSOS_3_IES = ["MAT1107_Coordinado - Macroseccion", "MAT1203_Coordinado - Macroseccion", "MAT1207_Coordinado - Macroseccion", "MAT1610_Coordinado - Macroseccion", "MAT1620_Coordinado - Macroseccion", "MAT1630_Coordinado - Macroseccion", "MAT1640_Coordinado - Macroseccion", "MAT251I_Coordinado - Macroseccion", "MAT253I_Coordinado - Macroseccion", "MAT255I_Coordinado - Macroseccion", "MAT2605_Coordinado - Macroseccion", "MAT380I_Coordinado - Macroseccion",
+                "EYP1025_Coordinado - Macroseccion", "EYP1113_Coordinado - Macroseccion", "EYP2114_Coordinado - Macroseccion", "EYP211I_Coordinado - Macroseccion", "EYP230I_Coordinado - Macroseccion"]
+#CURSOS_3_IES = ["MAT1640_Coordinado - Macroseccion","MAT1630_Coordinado - Macroseccion","MAT1620_Coordinado - Macroseccion","MAT1610_Coordinado - Macroseccion",
+#                "MAT1203_Coordinado - Macroseccion","EYP1113_Coordinado - Macroseccion"]
+
+
 #["EYP_MOD_MS1 - Macroseccion 1", "XN_MOD_MS1 - Macroseccion 1", "YF_MOD_MS1 - Macroseccion 1", "GCH_MOD_MS1 - Macroseccion 1", 
                 #"ED_MOD_MS1 - Macroseccion 1", "WV_MOD_MS1 - Macroseccion 1"]
 
 #Algunos parametros del modelo
 DELTAMIN = 28
 DELTAMAX = 63
-VACANTES = 2000
+VACANTES = 3000
 
 SEMANA_LICENCIATURA = mapeo(PATH_PARAMETROS, "Fechas", "H1")
 
 #siglas fmat, fis y qim hardcodeadas en filtracion_archivos modulos_mod_dipre
 
-INCLUIR_MAT = False
+INCLUIR_MAT = True
 INCLUIR_FIS_Y_QIM = False
 
-IDENTIFICADORES_FMAT = ["MAT1640", "MAT1630", "MAT1620", "MAT1610","MAT1207","EYP1113", "MAT1203"]
+IDENTIFICADORES_FMAT = ["MAT1107", "MAT1203", "MAT1207", "MAT1610", "MAT1620", "MAT1630", "MAT1640", "MAT251I", "MAT253I", "MAT255I", "MAT2605", "MAT380I",
+                        "EYP1025", "EYP1113", "EYP2114", "EYP211I", "EYP230I"]
+
 # IDENTIFICADORES_FMAT = ["EYP_MOD", "XN_MOD", "WV_MOD", "YF_MOD", "GCH_MOD", "ED_MOD"]
 
 IDENTIFICADORES_FIS_Y_QIM = ["FIS1514", "FIS1523", "FIS1533", "QIM100E"]
@@ -92,3 +98,14 @@ IDENTIFICADORES_FIS_Y_QIM = ["FIS1514", "FIS1523", "FIS1533", "QIM100E"]
 
 FECHA_INICIO_CLASES = datetime(2024, 8, 5)
 FECHA_FIN_CLASES = datetime(2024, 11, 29)
+
+COMPATIBLES = [ ["MAT1610", "MAT1620", "EYP211I", "MAT253I", "MAT380I", "MAT1107"],
+                ["MAT1203", "MAT255I"],
+                ["EYP1113", "MAT251I"],
+                ["EYP2114", "MAT1207", "MAT2605", "MAT1630"],
+                ["EYP1025", "EYP230I"],
+                ["EYP211I", "MAT253I", "MAT380I", "MAT1203"],
+                ["MAT1610", "MAT1620", "MAT255I", "MAT1107"],
+                ["EYP1113", "MAT251I"],
+                ["EYP2114", "MAT1640", "MAT1207"],
+                ["MAT1630", "MAT2605"]]
